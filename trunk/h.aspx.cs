f822943ce55c675f5c52eff5e69cb14242db2f87
stonedonkey <stonedonkey@ebc4f494-9c59-11dd-a29e-e3fa11317253>
2009-08-26 19:29:29 +0000

    Added UNF and TAG support .. UNF UNF!!

--- trunk/h.aspx.cs
+++ trunk/h.aspx.cs
@@ -18,49 +18,53 @@ using ShackToGo.Helper;
 
 public partial class h : System.Web.UI.Page
 {
-  protected void Page_Load(object sender, EventArgs e)
-  {
-    // handle ThomW LOL page
-    if (Request.QueryString["f"] == "LOL" || Request.QueryString["f"] == "INF")
+    protected void Page_Load(object sender, EventArgs e)
     {
-      try
-      {
-        string person = Request.QueryString["u"];
-        string threadid = Request.QueryString["i"];
-        string tag = Request.QueryString["f"];
+        // handle ThomW LOL page
+        if (Request.QueryString["f"] == "LOL" || Request.QueryString["f"] == "INF" || Request.QueryString["f"] == "UNF" || Request.QueryString["f"] == "TAG")
+        {
+            try
+            {
+                string person = Request.QueryString["u"];
+                string threadid = Request.QueryString["i"];
+                string tag = Request.QueryString["f"];
 
-        string sURL = String.Format("http://lmnopc.com/greasemonkey/shacklol/report.php?who={0}&what={1}&tag={2}&version=-1", person, threadid, tag);
+                string sURL = String.Format("http://lmnopc.com/greasemonkey/shacklol/report.php?who={0}&what={1}&tag={2}&version=-1", person, threadid, tag);
 
-        // try and load ThomW's page!
-        HttpWebRequest http = (HttpWebRequest)HttpWebRequest.Create(sURL);
-        http.Method = "GET";
+                // try and load ThomW's page!
+                HttpWebRequest http = (HttpWebRequest)HttpWebRequest.Create(sURL);
+                http.Method = "GET";
 
-        HttpWebResponse wr = (HttpWebResponse)http.GetResponse();
+                HttpWebResponse wr = (HttpWebResponse)http.GetResponse();
 
-        StreamReader sr = new StreamReader(wr.GetResponseStream());
+                StreamReader sr = new StreamReader(wr.GetResponseStream());
 
-        string result = sr.ReadToEnd();
+                string result = sr.ReadToEnd();
 
-        if (result != null && result.Length > 2)
-          if (result.Substring(0, 2) == "ok")
-          {
-            if (Request.QueryString["f"] == "LOL")
-              this.LiteralResult.Text = "Post [ <span class=\"l\">LOL'd</span> ]";
-            else
-              this.LiteralResult.Text = "Post [ <span class=\"i\">INF'd</span> ]";
-          }
-          else
-            this.LiteralResult.Text = result;
-        else
-          this.LiteralResult.Text = "Error LOLing or INFing - Internets Boo!";
-      }
-      catch (Exception)
-      {
-        this.LiteralResult.Text = "Error LOLing or INFing - Internets Boo!";
-      }
-      this.LiteralBackToChatty.Text = string.Format("<a href=\"{0}\">back to chatty</a>", Page.ResolveUrl("~") + Helper.AppendUserName("?"));
-      this.LiteralBackToThread.Text = string.Format("<a href=\"t.aspx?i={0}&s={1}{2}\">back to thread</a>", Request.QueryString["i"], Request.QueryString["s"], Helper.AppendUserName("&"));
+                if (result != null && result.Length > 2)
+                    if (result.Substring(0, 2) == "ok")
+                    {
+                        if (Request.QueryString["f"] == "LOL")
+                            this.LiteralResult.Text = "Post [ <span class=\"l\">LOL'd</span> ]";
+                        else if (Request.QueryString["f"] == "UNF")
+                            this.LiteralResult.Text = "Post [ <span class=\"u\">UNF'd</span> ]";
+                        else if (Request.QueryString["f"] == "TAG")
+                            this.LiteralResult.Text = "Post [ <span class=\"t\">TAG'd</span> ]";
+                        else
+                            this.LiteralResult.Text = "Post [ <span class=\"i\">INF'd</span> ]";
+                    }
+                    else
+                        this.LiteralResult.Text = result;
+                else
+                    this.LiteralResult.Text = "Error LOLing or INFing or TAGing or UNFing - Internets Boo!";
+            }
+            catch (Exception)
+            {
+                this.LiteralResult.Text = "Error LOLing or INFing or TAGing or UNFing  - Internets Boo!";
+            }
+            this.LiteralBackToChatty.Text = string.Format("<a href=\"{0}\">back to chatty</a>", Page.ResolveUrl("~") + Helper.AppendUserName("?"));
+            this.LiteralBackToThread.Text = string.Format("<a href=\"t.aspx?i={0}&s={1}{2}\">back to thread</a>", Request.QueryString["i"], Request.QueryString["s"], Helper.AppendUserName("&"));
+        }
+
     }
-
-  }
 }
